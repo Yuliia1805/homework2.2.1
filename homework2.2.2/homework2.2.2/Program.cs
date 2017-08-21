b96c891b7b6aee9481485f8221a6873ec725a7ef
Yuliia <@DESKTOP-6222HOQ>
2017-08-22 00:52:46 +0300

    init commit

---
+++ homework2.2.2/homework2.2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal x;
+            int a = 20;
+            decimal b = decimal.Divide(4, 5);
+
+            x = a / b;
+
+            Console.WriteLine("Result is: " + x);
+
+            Console.ReadLine();
+        }
+    }
+}
